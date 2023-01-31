72057909b1166980da316aade075b205228fa183
Neslisah <neslisah.celek@outlook.com>
2023-01-31 13:45:25 +0300

    unnecessary files deleted

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { /** **/
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
